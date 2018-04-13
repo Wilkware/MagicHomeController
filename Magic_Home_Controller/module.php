a95eca948eed339a835f2018ef14bc0acd71adae
Wilkware <heiko.wilknitz@wilkware.de>
2018-04-13 22:39:11 +0200

    Initialversion

---
+++ Magic_Home_Controller/module.php
@@ -0,0 +1,483 @@
+<?
+/**
+ * MagicHomeControl ist die Klasse für das IPS-Modul 'SymconMHC'.
+ * Erweitert IPSModule 
+ */
+class MagicHomeControl extends IPSModule
+{
+  // Modul 
+  var $moduleName = "Magic Home Control";
+  // Profil arrays
+  var $assoGRB =  array(
+    array(0,  "Manuell", "", 0x000000),
+    array(37, "7-stufiger Farbdurchlauf", "", 0x000000),
+    array(38, "Rot pulsierend", "", 0xFF0000),
+    array(39, "Grün pulsierend", "", 0x00FF00),
+    array(40, "Blau pulsierend", "", 0x0000FF),
+    array(41, "Gelb pulsierend", "", 0xFFFF00),
+    array(42, "Türkis pulsierend", "", 0x00FFFF),
+    array(43, "Violett pulsierend", "", 0xFF00FF),
+    array(44, "Rot Grün pulsierend", "", 0xF0F000),
+    array(45, "Rot Blau pulsierend", "", 0xF000F0),
+    array(46, "Grün Blau pulsierend", "", 0x00F0F0),
+    array(47, "7-stufig blitzend", "", 0xA0A0A0),
+    array(48, "Rot blitzend", "", 0xFF0000),
+    array(49, "Grün blitzend", "", 0x00FF00),
+    array(50, "Blau blitzend", "", 0x0000FF),
+    array(51, "Gelb blitzend", "", 0xFFFF00),
+    array(52, "Türkis blitzend", "", 0x00FFFF),
+    array(53, "Violett blitzend", "", 0xFF00FF),
+    array(54, "Weiss blitzend", "", 0xFFFFFF),
+    array(55, "7-stufiger Farbwechsel", "", 0xA0A0A0)
+  );			
+  var $assoBRG =  array(
+    array(0,  "Manuell", "", 0x000000),
+    array(37, "7-stufiger Farbdurchlauf", "", 0x000000),
+    array(38, "Rot pulsierend", "", 0xFF0000),
+    array(39, "Blau pulsierend", "", 0x0000FF),
+    array(40, "Grün pulsierend", "", 0x00FF00),
+    array(41, "Violett pulsierend", "", 0xFF00FF),
+    array(42, "Türkis pulsierend", "", 0x00FFFF),
+    array(43, "Gelb pulsierend", "", 0xFFFF00),
+    array(44, "Rot Grün pulsierend", "", 0xF0F000),
+    array(45, "Rot Blau pulsierend", "", 0xF000F0),
+    array(46, "Grün Blau pulsierend", "", 0x00F0F0),
+    array(47, "7-stufiger Farbwechsel", "", 0xA0A0A0),	
+    array(48, "7-stufig blitzend", "", 0xA0A0A0),
+    array(49, "Rot blitzend", "", 0xFF0000),
+    array(50, "Blau blitzend", "", 0x0000FF),
+    array(51, "Grün blitzend", "", 0x00FF00),
+    array(52, "Violett blitzend", "", 0xFF00FF),
+    array(53, "Türkis blitzend", "", 0x00FFFF),
+    array(54, "Gelb blitzend", "", 0xFFFF00),
+    array(55, "Weiss blitzend", "", 0xFFFFFF)			
+  );
+
+  /**
+   * Create.
+   *
+   * @access public
+   */
+  public function Create()
+  {
+    // Never delete this line!
+    parent::Create();
+
+    // Config Variablen
+    $this->RegisterPropertyString("TCPIP", "127.0.0.1");
+    $this->RegisterPropertyString("RGB", "012");
+    $this->RegisterPropertyBoolean("LOG", false);
+
+    // Variablen Profile einrichten
+    $this->RegisterProfile(IPSVarType::vtInteger, "MHC.ModeGRB", "Bulb", "", "", 0, 0, 0, 0, $assoGRB);
+    $this->RegisterProfile(IPSVarType::vtInteger, "MHC.ModeBRG", "Bulb", "", "", 0, 0, 0, 0, $assoBRG);
+
+    // Variablen erzeugen
+    $varID = $this->RegisterVariableBoolean("Power", "Aktiv");
+    IPS_SetVariableCustomProfile($varID,"~Switch");
+    $this->EnableAction("Power"); 
+    
+    $varID = $this->RegisterVariableInteger("Speed", "Geschwindigkeit");
+    IPS_SetVariableCustomProfile($varID,"~Intensity.100");
+    $this->EnableAction("Speed"); 
+    
+    $varID = $this->RegisterVariableInteger("Mode", "Modus");
+    IPS_SetVariableCustomProfile($varID,"MHC.ModeGRB");
+    $this->EnableAction("Mode"); 
+    
+    $varID = $this->RegisterVariableInteger("Color", "Farbe");
+    IPS_SetVariableCustomProfile($varID,"~HexColor");
+    $this->EnableAction("Color"); 
+    
+    $varID = $this->RegisterVariableInteger("Brightness", "Helligkeit");
+    IPS_SetVariableCustomProfile($varID,"~Intensity.100");
+    $this->EnableAction("Brightness"); 
+  }
+
+  /**
+   * Destroy.
+   *
+   * @access public
+   */
+  public function Destroy()
+  {
+      // Never delete this line!
+      parent::Destroy();
+  }
+
+  /**
+   * Apply Configuration Changes.
+   *
+   * @access public
+   */
+  public function ApplyChanges()
+  {
+    // Never delete this line!
+    parent::ApplyChanges();
+
+    // Debug message
+    $tcpIP  = $this->ReadPropertyString("TCPIP");
+    $rgbID  = $this->ReadPropertyString("RGB");
+    $log    = $this->ReadPropertyBoolean("LOG");
+    $this->SendDebug("ApplyChanges", "IP=".$tcpIP.", RGB=".$$rgbID.", LOG=".(int)$LOG, 0);
+
+    // Debug to Loging
+ 		if ($log) {
+		  IPS_LogMessage($this->moduleName,"ApplyChanges: IP=".$tcpIP.", RGB=".$$rgbID.", LOG=".(int)$LOG);
+    }
+
+    // IP Check
+    if (filter_var($this->ReadPropertyString("TCPIP"), FILTER_VALIDATE_IP) !== false) {
+    	$this->SetStatus(102);
+    }
+    else {
+    	$this->SetStatus(201);
+    }
+  
+    // Setup variable profil
+    $varID = $this->GetIDForIdent("Mode");
+    if ($rgbID == "012") {
+      IPS_SetVariableCustomProfile($varID, "MHC.ModeGRB");
+    }
+    else {
+      IPS_SetVariableCustomProfile($varID, "MHC.ModeBRG");
+    }
+  }
+
+  /**
+   * Call by visual changes
+   *
+   * @access public
+   */
+  public function RequestAction($ident, $value)
+  {
+    // Debug & Logging
+    $this->SendDebug("RequestAction", "RequestAction: ($ident,$value)", 0);
+    if ($this->ReadPropertyBoolean("LOG")) {
+    	IPS_LogMessage($this->moduleName,"RequestAction: ($ident,$value)");
+    }
+	 	 
+    switch($Ident) {
+      // Switch Power On/Off
+      case "Power":
+				$on   = array(0x71,0x23,0x0f);
+				$off  = array(0x71,0x24,0x0f);
+				if ($value) {
+					$this->SendData($on);
+				}
+				else {
+					$this->SendData($off);
+				}
+				SetValue($this->GetIDForIdent($ident), $value);
+				break;
+      // Set Speed value
+			case "Speed":
+				SetValue($this->GetIDForIdent($ident), $value);
+				$this->SendFunction();
+				break;
+      // Set Display Mode
+			case "Mode":
+				IPS_SetDisabled($this->GetIDForIdent("Speed"),!$value);
+				IPS_SetDisabled($this->GetIDForIdent("Color"),$value);
+				IPS_SetDisabled($this->GetIDForIdent("Brightness"),$value);
+				SetValue($this->GetIDForIdent($ident), $value);
+				// Manual mode. 
+				if ($Value == 0) {
+					$this->SendColor();
+        }
+				// Functional mode
+        else {
+					$this->SendFunction();
+        }
+				break;
+			case "Color":
+			case "Brightness":
+				SetValue($this->GetIDForIdent($ident), $value);
+				$this->SendColor();
+				break;
+			default:
+				throw new Exception("Invalid Ident");
+		}
+	}
+  
+  /**
+   * Create the profile for the given associations.
+   *
+   * @access protected
+   */
+	protected function RegisterProfile($vartype, $name, $icon, $prefix = "", $suffix = "", $minvalue = 0, $maxvalue = 0, $stepsize = 0, $digits = 0, $associations = NULL)
+	{
+		if (!IPS_VariableProfileExists($name))
+		{
+			switch ($vartype)
+			{
+				case IPSVarType::vtBoolean:
+					$this->RegisterProfileBoolean($name, $icon, $prefix, $suffix, $associations);
+					break;
+				case IPSVarType::vtInteger:
+					$this->RegisterProfileInteger($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $stepsize, $digits, $associations);
+					break;
+				case IPSVarType::vtFloat:
+					$this->RegisterProfileFloat($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $stepsize, $digits, $associations);
+					break;
+				case IPSVarType::vtString:
+					$this->RegisterProfileString($name, $icon);
+					break;
+			}	
+		}
+		return $name;
+	}
+
+  /**
+   * RegisterProfileType
+   *
+   * @access protected
+   */
+	protected function RegisterProfileType($name, $type)
+	{
+    if(!IPS_VariableProfileExists($name)) {
+      IPS_CreateVariableProfile($name, $type);
+    } 
+    else {
+      $profile = IPS_GetVariableProfile($name);
+      if($profile['ProfileType'] != $type)
+        throw new Exception("Variable profile type does not match for profile ".$name);
+    }
+  }
+
+  /**
+   * RegisterProfileBoolean
+   *
+   * @access protected
+   */
+	protected function RegisterProfileBoolean($name, $icon, $prefix, $suffix, $asso)
+	{
+    $this->RegisterProfileType($name, IPSVarType::vtBoolean);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+  /**
+   * RegisterProfileInteger
+   *
+   * @access protected
+   */
+	protected function RegisterProfileInteger($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $step, $digits, $asso)
+	{
+    $this->RegisterProfileType($name, IPSVarType::vtInteger);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+    IPS_SetVariableProfileDigits($name, $digits);
+
+    if(sizeof($asso) === 0){
+      $minvalue = 0;
+      $maxvalue = 0;
+    } 
+    IPS_SetVariableProfileValues($name, $minvalue, $maxvalue, $step);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+  /**
+   * RegisterProfileFloat
+   *
+   * @access protected
+   */
+	protected function RegisterProfileFloat($name, $icon, $prefix, $suffix, $minvalue, $maxvalue, $step, $digits, $asso)
+	{
+    $this->RegisterProfileType($name, IPSVarType::vtFloat);
+    
+    IPS_SetVariableProfileIcon($name, $icon);
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+    IPS_SetVariableProfileDigits($name, $digits);
+
+    if(sizeof($asso) === 0){
+      $minvalue = 0;
+      $maxvalue = 0;
+    } 
+    IPS_SetVariableProfileValues($name, $minvalue, $maxvalue, $step);
+
+    if(sizeof($asso) !== 0){
+      foreach($asso as $ass) {
+        IPS_SetVariableProfileAssociation($name, $ass[0], $ass[1], $ass[2], $ass[3]);
+      }
+    }         
+  }
+
+  /**
+   * RegisterProfileString
+   *
+   * @access protected
+   */
+	protected function RegisterProfileString($name, $icon, $prefix, $suffix)
+	{
+    $this->RegisterProfileType($name, IPSVarType::vtString);
+
+    IPS_SetVariableProfileText($name, $prefix, $suffix);
+		IPS_SetVariableProfileIcon($name, $icon);
+  }
+
+  /**
+   * RegisterVariable
+   * 
+   * @access protected
+   */
+	protected function RegisterVariable($vartype, $name, $ident, $profile, $position, $register)
+	{
+		if($register == true) {
+			switch ($vartype) {
+				case IPSVarType::vtBoolean:
+					$objId = $this->RegisterVariableBoolean($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtInteger:
+					$objId = $this->RegisterVariableInteger($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtFloat:
+					$objId = $this->RegisterVariableFloat($ident, $name, $profile, $position);
+					break;
+				case IPSVarType::vtString:
+					$objId = $this->RegisterVariableString($ident, $name, $profile, $position);
+					break;
+			}	
+		}
+		else {
+			$objId = @$this->GetIDForIdent($ident);
+			if ($objId > 0) {
+				$this->UnregisterVariable($ident);
+			}
+		}
+		return $objId;
+	}
+  
+
+  /**
+   * Send function data
+   *
+   * @access privat
+   */
+	private function SendFunction()
+	{
+		$data   = array(0x61,0x00,0x00,0x0F);
+		$mode   = GetValue($this->GetIDForIdent("Mode"));
+		$speed  = 100 - GetValue($this->GetIDForIdent("Speed"));
+
+		if($speed > 100) {
+			$speed = 100;
+    }
+		else if($speed < 1) {
+			$speed = 1;
+    }
+		
+		$data[1] = $mode;
+		$data[2] = $speed;
+	
+		$this->SendData($data);
+	}
+	
+  /**
+   * Send color data
+   *
+   * @access privat
+   */
+	private function SendColor()
+	{
+		$data = array(0x31,0x00,0x00,0x00,0x00,0x00,0x0F);
+
+		$brightness = GetValue($this->GetIDForIdent("Brightness")) / 100;
+		$color  = GetValue($this->GetIDForIdent("Color"));
+		
+    $rgb  = array(0x00, 0x00, 0x00);
+		$rgb[0] = (($color >> 16) & 0xFF); // red
+		$rgb[1] = (($color >> 8) & 0xFF); // green
+		$rgb[2] = ($color & 0xFF);  // blue
+		
+		// map with brightness
+		$rgb[0] *= $brightness;
+		$rgb[1] *= $brightness;
+		$rgb[2] *= $brightness;
+
+    // map rgb channel
+    $channel = $this->ReadPropertyString("RGB");
+    
+		$data[1] = floor($rgb[(int)$channel[0]);
+		$data[2] = floor($rgb[(int)$channel[1]);
+		$data[3] = floor($rgb[(int)$channel[2]);
+    
+		$this->SendData($data);
+	}
+	
+  /**
+   * Send data array to controller
+   *
+   * @access privat
+   */
+	private function SendData($values)
+	{
+		$path   = "tcp://".$this->ReadPropertyString("TCPIP");
+		$socket = @fsockopen($path, 5577, $errno, $errstr, 5);
+    $log    = $this->ReadPropertyBoolean("LOG");
+
+    // Check Socket 
+		if(!$socket) {
+      $this->SendDebug("SendData", $path." -> $errstr ($errno)", 0);
+   		if ($log) {
+  		  IPS_LogMessage($this->moduleName,"SendData: ". $path." -> $errstr ($errno)");
+      }
+			return;
+		}
+		else {
+      $this->SendDebug("SendData", "Verbindung aufgebaut '".$path."'", 0);
+   		if ($log) {
+  			IPS_LogMessage($this->moduleName,"SendData: Verbindung aufgebaut '".$path."'");
+      }
+    }
+			
+		$send = "";
+		foreach($values as $value) {
+			$send  .= chr($value);
+			$data[] = $value;
+		}
+		$check = $this->GetChecksum($values);
+		
+		$send  .= chr($check);
+		$data[] = $check;
+		
+    // send data
+		fwrite($socket, $send);
+
+    $this->SendDebug("SendData", "Sende Daten=".join(",",$data), 0);
+ 		if ($log) {
+		  IPS_LogMessage($this->moduleName,"SendData: Sende Daten=".join(",",$data));
+    }
+    // close socket		
+		fclose($socket);
+	}
+	
+  /**
+   * Calculatue checksum for given data
+   *
+   * @access privat
+   */
+	private function GetChecksum($values)
+	{
+		$checksum = array_sum($values);	
+		$checksum = dechex($checksum);
+		$checksum = substr($checksum, -2);
+		$checksum = hexdec($checksum);
+		
+		return $checksum;
+	}
+}
+?>
---
+++ README.md
@@ -0,0 +1,102 @@
+[![Version](https://img.shields.io/badge/Symcon-PHP--Modul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Version](https://img.shields.io/badge/Symcon%20Version-5.0-blue.svg)](https://www.symcon.de/produkt/)
+[![Version](https://img.shields.io/badge/Modul%20Version-1.0.20180415-orange.svg)](https://github.com/Wilkware/SymconMHC)
+[![Version](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+# MHC - Magic Home Controller
+IP-Symcon Modul für die Ansteuerung von WiFi LED Controller der Firma _Magic Home_.
+
+### Danksagung 
+
+Dieses Modul basiert auf dem Modul von ...
+* _Spoosie_ : Modul _KH\_LEDWiFiController_ (https://github.com/Spoosie/KH_LEDWiFiController) 
+
+Vielen Dank für die hervorragende und tolle Arbeit! 
+
+### Inhaltverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Software-Installation](#3-software-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#5-statusvariablen-und-profile)
+6. [WebFront](#6-webfront)
+7. [PHP-Befehlsreferenz](#7-php-befehlsreferenz)
+8. [Changlog] (#8-changelog)
+
+### 1. Funktionsumfang
+
+Das Modul dient zur Ansteuerung von LED Strpes mittels eines WiFi LED Controllers des Herstellers Magic Home.
+Das sind Controller vom Typ LD382 bzw. LD382A.
+
+
+Wenn jemand noch weitere kennt, bitte einfach bei mir melden!
+
+### 2. Voraussetzungen
+
+- IP-Symcon Version 5.0 
+- Funktioniert wahrscheinlich auch mit der Version 4.4 (nicht getestet).
+
+### 3. Software-Installation
+
+Über das Modul-Control folgende URL hinzufügen.  
+`https://github.com/Wilkware/SymconMHC` oder `git://github.com/Wilkware/SymconMHC.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+- Unter "Instanz hinzufügen" ist das 'Magic Home Controller'-Modul (Alias: LED Controller) unter dem Hersteller '(Sonstige)' aufgeführt.
+
+__Konfigurationsseite__:
+
+Die Konfiguration beinhaltet die Zuweisung der IP-Adresse und der Auswahl der Farbkanal-Reihenfolge (RGB-Pins).
+Derzeit werden Stripes mit der Reihenfolge GRB (Gelb, Rot, Blau) und BRG (Blau, Rot, Gelb) unterstützt. 
+Die Reihenfolge der Pins beginnt immer ausgehend vom GND-Pin (12V Pin).
+
+Darüber hinaus kann noch ausgewählt werden, ob die Debug-Meldungen auch im Status Logging (Log Meldungen) ausgegeben werden sollen.
+
+Name               | Beschreibung
+------------------ | ---------------------------------
+WiFi Controller IP | IP-Adresse des Controlers im lokalen WLAN (zb. 192.168.0.10)
+RGB Pin Belegung   | Reihenfolge der Farb-Pins (GRB oder BRG), Standard ist GRB
+Meldungen im Log   | Ausgabe von Debug-Meldungen im Log
+
+
+### 5. Statusvariablen und Profile
+
+Ident         | Name                | Typ       |  Profil                      | Beschreibung
+--------------|-------------------- | --------- | --------------------------------------------------------------------------------------
+Brightness    | Helligkeit          | Integer   | ~Intensity.100               | Helligkeitswert von 0 bis 100%
+Color         | Farbe               | Integer   | ~HexColor                    | Farbwert
+Mode          | Modus               | Integer   | MHC.ModeGRB oder MHC.ModeBRG | Manueller Farbmodus oder vordefinierter Funktionsmodus
+Power         | Aktiv               | Boolean   | ~Switch                      | An/Aus Schalter 
+Speed         | Geschwindigkeit     | Integer   | ~Intensity.100               | Geschwindigkeitswert von 0 bis 100%
+
+### 6. WebFront
+
+Man kann die Statusvariaben direkt im WebFront verlinken.
+
+### 7. PHP-Befehlsreferenz
+
+`void MHC_SetBrightness(int $InstanzID, int $Brightness);` 
+Setzt die Helligkeit auf $Brightness. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetColor(int $InstanzID, int $Color);` 
+Setzt den Farbwert auf $Color. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetMode(int $InstanzID, int $Mode);` 
+Setzt den Anzeigemodus auf auf $Mode. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_Power(int $InstanzID, bool $Power);` 
+Schaltet den Controler Ein(true) bzw Aus(false). Die Funktion liefert keinerlei Rückgabewert.
+
+### 8. Changelog
+
+Version 20180415
+* _NEU_: Initialversion
+
+### Entwickler
+* Heiko Wilknitz ([@wilkware](https://github.com/wilkware))
+
+### Spenden
+Die Software ist für die nicht kommzerielle Nutzung kostenlos, Schenkungen als Unterstützung für den Entwickler bitte hier:  
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8816166" target="_blank"><img src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" /></a>
